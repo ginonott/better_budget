d0842b8854a03c3c1c7675033517a34e2c088ba8
Gino Notto <gnotto@bloomberg.net>
2019-07-16 16:37:27 -0400

    * clean slate

--- src/constants/database.js
+++
@@ -1,4 +0,0 @@
-export const TRANSACTIONS = 'transactions';
-export const BUDGETS = 'budgets';
-export const SCHEDULED = 'scheduledTransactions';
-export const LOG = 'log';
--- src/constants/status.js
+++
@@ -1,8 +0,0 @@
-const STATUSES = {
-    NOT_STARTED: 'NOT_STARTED',
-    STARTED: 'STARTED',
-    FAILED: 'FAILED',
-    FINISHED: 'FINISHED'
-}
-
-export default STATUSES;
